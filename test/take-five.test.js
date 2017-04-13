d07f59bebb7f03c68538b39fa0f3b637c4294685
nicky evers <livelyevers@gmail.com>
2017-04-13 10:42:31 -0700

    moved elapse to correct place

--- test/take-five.test.js
+++ test/take-five.test.js
@@ -2,12 +2,11 @@ const assert = require('assert');
 const takeFive = require('../lib/take-five');
 
 it('takes five seconds', done => {
-  this.timeout(7000);
   const start = new Date();
-  const elapsed = new Date - start;
-  takeFive(start, err => {
-    if(err) return done(err);
+
+  takeFive(() => {
+    const elapsed = new Date - start;
     assert.ok(elapsed > 5000 && elapsed < 6000);
     done();
   });
-});
+}).timeout(7000);
